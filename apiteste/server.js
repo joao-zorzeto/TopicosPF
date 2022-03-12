ac5d486e312ee0167414a35073d99c58787fa344
Joao Pedro Zorzeto <186246@upf.br>
2022-03-11 21:35:24 -0300

    Commit de 11/03/2022-21h35m23s

--- apiteste/server.js
+++ apiteste/server.js
@@ -1,24 +1,23 @@
-console.log("Arquivo server.js executou com sucesso!");
+console.log("server 123 rodando")
 
-//usar o express
 const express = require ('express');
 const app = express();
-app.use(express.json()); //para tratar json
+app.use(express.json());
+
 
 // conexão com mongoBD
 const MongoClient = require('mongodb').MongoClient;
 const ObjectId = require('mongodb').ObjectId;
 const uri = "mongodb://admin:admin@localhost:27018/teste?authSource=teste ";
 MongoClient.connect(uri, (err, client) => {
-    if (err)
-        return console.log(err);
-    db = client.db('teste');
-    app.listen(3000, function() { // subir serviço da api na porta 3000
-        console.log('API rodando na porta 3000');
-        console.log('Testar por http://localhost:3000');
-    });
+ if (err)
+ return console.log(err);
+ db = client.db('meuteste');
+ app.listen(3000, function() { // subir serviço da api na porta 3000
+ console.log('API rodando na porta 3000');
+ console.log('Testar por http://localhost:3000');
  });
-
+ });
  // prerarar para responder ao GET
 app.get('/', (req, res) => {
     res.send('Atendida a requisição GET!!');
@@ -26,6 +25,9 @@ app.get('/', (req, res) => {
 
 // rotas para animais
 const animais = require('./animais');
-animais(app);   
+animais(app);
 
 
+// rotas para animais
+const colaborador = require('./colaborador');
+colaborador(app);
