0ec1c90f3ad51aaa0babe1ed20aa75a7a7766ce7
Pedro Zorzeto <186246@upf.br>
2022-03-25 21:58:38 -0300

    Commit de 25/03/2022-21h58m37s

--- apirequisicoes/RodarBaseRequisicoes.bat
+++ apirequisicoes/RodarBaseRequisicoes.bat
@@ -1,1 +1,2 @@
-"C:\Program Files\MongoDB\Server\5.0\bin\mongod" --port 27018 --dbpath .\basesmongo\apirequisicoes --auth
+"C:\Program Files\MongoDB\Server\5.0\bin\mongod" --port 27018 --dbpath ..\basesmongo\baserequisicoes --auth
+
--- apirequisicoes/RodarCliMongo.bat
+++ apirequisicoes/RodarCliMongo.bat
@@ -1,1 +1,1 @@
-"C:\Program Files\MongoDB\Server\5.0\bin\mongo" -u "admin" -p "admin" --port 27018 --authenticationDatabase "apirequisicoes"
+"C:\Program Files\MongoDB\Server\5.0\bin\mongo"  --port 27018
